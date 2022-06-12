4cc6dbe61f6aa88383ac0a9553c76eb5f7ed2a80
robertokbr <robertojuniordev@gmail.com>
2022-06-12 04:10:01 -0300

    feat: add onlyOwner modfier

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,7 +10,17 @@ contract FundMe {
     uint256 minimumUSD = 50 * 1e18;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-    
+    address public i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == i_owner);
+        _;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUSD, "The minimum fund value is 50 USD");
 
@@ -18,7 +28,7 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public payable {
+    function withdraw() public onlyOwner payable {
         for (uint256 i = 0; i < funders.length; i++) {
             addressToAmountFunded[funders[i]] = 0;
         }
@@ -27,6 +37,6 @@ contract FundMe {
 
         (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }("");
     
-        require(callSuccess, "Transaction has failed");
+        require(callSuccess, "Call has failed");
     }
 }
