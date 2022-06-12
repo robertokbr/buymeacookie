5b71b29fb482d7dc64c6884261625fea17b12931
robertokbr <robertojuniordev@gmail.com>
2022-06-12 03:42:34 -0300

    feat: add fund method

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,10 +2,19 @@
 
 pragma solidity ^0.8.8;
 
+import "./PriceConverter.sol";
+
 contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 minimumUSD = 50 * 1e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    
     function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUSD, "The minimum fund value is 50 USD");
 
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
-
-    function withdraw() public payable {}
 }
