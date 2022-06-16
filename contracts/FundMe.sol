1978cf16392ff04534b54070270b32ba00f27a32
robertokbr <robertojuniordev@gmail.com>
2022-06-15 22:03:21 -0300

    refactor: move chainlink pricefeed address out of contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,47 +5,49 @@ pragma solidity ^0.8.8;
 import "./PriceConverter.sol";
 
 contract FundMe {
-    using PriceConverter for uint256;
+  using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 50 * 1e18;
-    address[] public funders;
-    mapping(address => uint256) public addressToAmountFunded;
-    address public immutable i_owner;
+  uint256 public constant MINIMUM_USD = 50 * 1e18;
+  address[] public funders;
+  mapping(address => uint256) public addressToAmountFunded;
+  address public immutable i_owner;
+  AggregatorV3Interface public priceFeed;
 
-    constructor() {
-        i_owner = msg.sender;
+  constructor(address priceFeedAddress) {
+    i_owner = msg.sender;
+    priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  modifier onlyOwner{
+    require(msg.sender == i_owner);
+    _;
+  }
+
+  function fund() public payable {
+    require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "The minimum fund value is 50 USD");
+
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] += msg.value;
+  }
+
+  function withdraw() public onlyOwner payable {
+    for (uint256 i = 0; i < funders.length; i++) {
+        addressToAmountFunded[funders[i]] = 0;
     }
 
-    modifier onlyOwner{
-        require(msg.sender == i_owner);
-        _;
-    }
+    funders = new address[](0);
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "The minimum fund value is 50 USD");
+    (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }("");
 
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
-    }
+    require(callSuccess, "Call has failed");
+  }
 
-    function withdraw() public onlyOwner payable {
-        for (uint256 i = 0; i < funders.length; i++) {
-            addressToAmountFunded[funders[i]] = 0;
-        }
+  // Prevent wrong fund calls
+  receive() external payable {
+    fund();
+  }
 
-        funders = new address[](0);
-
-        (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }("");
-    
-        require(callSuccess, "Call has failed");
-    }
-
-    // Prevent wrong fund calls
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
-    }
+  fallback() external payable {
+    fund();
+  }
 }
--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -5,28 +5,35 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getLatestETHPrice() internal view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+  function getLatestETHPrice(
+    AggregatorV3Interface priceFeed
+  )
+    internal
+    view
+    returns (uint256)
+  {
+    (
+      /*uint80 roundID*/,
+      int price,
+      /*uint startedAt*/,
+      /*uint timeStamp*/,
+      /*uint80 answeredInRound*/
+    ) = priceFeed.latestRoundData();
 
-        (
-            /*uint80 roundID*/,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
-    
-        return uint256(price * 1e10);
-    }
+    return uint256(price * 1e10);
+  }
 
-    function getConversionRate(uint256 ethAmount)
-        internal
-        view
-        returns (uint256)
-    {
-        uint256 ethPrice = getLatestETHPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
-        return ethAmountInUsd;
-    }
+  function getConversionRate(
+    uint256 ethAmount,
+    AggregatorV3Interface priceFeed
+  )
+    internal
+    view
+    returns (uint256)
+  {
+    uint256 ethPrice = getLatestETHPrice(priceFeed);
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+    // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+    return ethAmountInUsd;
+  }
 }
