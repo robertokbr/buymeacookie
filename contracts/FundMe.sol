42b76e3c9f6c28b9587c06831528d563fa46fa14
robertokbr <robertojuniordev@gmail.com>
2022-06-17 17:05:56 -0300

    refactor: apply good practices to smart contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+error FundMe__NotOwner();
+
+/** @title A contract for crowd funding
+ *  @author Robertokbr
+ *  @notice This contract is to demo a sample funding contract
+ *  @dev This implements Chainlink Price feeds withing the PriceConverter lib
+ */
 contract FundMe {
   using PriceConverter for uint256;
 
@@ -13,16 +20,27 @@ contract FundMe {
   address public immutable i_owner;
   AggregatorV3Interface public priceFeed;
 
+  modifier onlyOwner{
+    if (msg.sender != i_owner) revert FundMe__NotOwner();
+    _;
+  }
+
   constructor(address priceFeedAddress) {
     i_owner = msg.sender;
     priceFeed = AggregatorV3Interface(priceFeedAddress);
   }
 
-  modifier onlyOwner{
-    require(msg.sender == i_owner);
-    _;
+  receive() external payable {
+    fund();
   }
 
+  fallback() external payable {
+    fund();
+  }
+
+  /**
+   *  @notice This function funds this contract and create a realtion between sender and funds
+   */
   function fund() public payable {
     require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "The minimum fund value is 50 USD");
 
@@ -40,14 +58,5 @@ contract FundMe {
     (bool callSuccess,) = payable(msg.sender).call{ value: address(this).balance }("");
 
     require(callSuccess, "Call has failed");
-  }
-
-  // Prevent wrong fund calls
-  receive() external payable {
-    fund();
-  }
-
-  fallback() external payable {
-    fund();
   }
 }
